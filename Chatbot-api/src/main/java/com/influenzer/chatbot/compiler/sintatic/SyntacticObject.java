a3c33999477248a6653ddcebc7bc32e08c9fe2d7
Lucas <lucashsilva99@gmail.com>
2019-05-22 16:34:42 -0300

    Implementação do otimizador Correção de bugs Issue #2

--- Chatbot-api/src/main/java/com/influenzer/chatbot/compiler/sintatic/SyntacticObject.java
+++ Chatbot-api/src/main/java/com/influenzer/chatbot/compiler/sintatic/SyntacticObject.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class SyntacticObject {
     
     private MessageType type;
+    private List<String> tokens;
     private List<SymbolObject> symbols = new ArrayList<>();
     
 }
